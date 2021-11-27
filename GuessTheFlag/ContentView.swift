0ec5b1ed83aea830e9835c8c30dbfdda53285c4f
OmarBakry-eg <OmarBakry202020@gmail.com>
2021-11-27 17:17:20 +0200

    after reskin

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -4,14 +4,6 @@
 //
 //  Created by Omar Bakry on 27/11/2021.
 
-// Color.primary used for text.
-// ignoresSafeArea() func.
-// VStack, HStack and ZStack.
-// .background(.ultraThinMaterial) like raised button background in flutter.
-// .foregroundStyle could make a vibrance effect.
-// Linear gradient, radial and angular.
-// button role .destructive make it red like delete
-// renderingMode(.original) show the origin color to the image
 
 import SwiftUI
 
@@ -23,22 +15,35 @@ struct ContentView: View {
 
     var body: some View {
         ZStack{
-            LinearGradient(gradient: Gradient(colors: [.blue,.black]), startPoint: .top, endPoint: .bottom).ignoresSafeArea()
-            VStack(spacing:30){
-                VStack {
-                    Text("Tap the flag of").foregroundColor(.white).font(.subheadline.weight(.heavy))
-                    Text(countries[correctAnswer]).foregroundColor(.white).font(.largeTitle.weight(.semibold))
-                }
-             
-                ForEach(0..<3){ num in
-                    Button {
-                       flagTapped(num)
-                    } label :{
-                        Image(countries[num]).renderingMode(.original).clipShape(Capsule()).shadow(radius:5)
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                  .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 700)
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Text("Guess The Flag").font(.largeTitle.weight(.bold))
+                    .foregroundColor(.white)
+                VStack(spacing:15){
+                    VStack {
+                        Text("Tap the flag of").foregroundStyle(.secondary).font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer]).font(.largeTitle.weight(.semibold))
                     }
-                    
-                }
-            }
+                 
+                    ForEach(0..<3){ num in
+                        Button {
+                           flagTapped(num)
+                        } label :{
+                            Image(countries[num]).renderingMode(.original).clipShape(Capsule()).shadow(radius:5)
+                        }
+                        
+                    }
+                }.frame(maxWidth:.infinity).padding(.vertical,20).background(.regularMaterial).clipShape(RoundedRectangle(cornerRadius: 20))
+                Spacer()
+                Spacer()
+                Text("Score : \(scoreTitle.isEmpty ? "No Score for now" : scoreTitle == "Correct" ? "UP" : "DOWN")").foregroundColor(.white).font(.title.bold())
+                Spacer()
+            }.padding()
         }.alert(scoreTitle, isPresented: $showingScore)
         {
             Button("Continue",action: askQuestion)
